271f912b6d2f9005aaefd799f93551edb1b9931b
Greg Babula <gbabula@gmail.com>
2015-03-30 21:55:55 -0400

    user service / WIP

--- app/scripts/controllers/user-private-profile.js
+++ app/scripts/controllers/user-private-profile.js
@@ -1,19 +1,18 @@
 /**
- * 
+ *
  * @module user-private-profile
  * @author Greg Babula [gbabula@gmail.com]
- * @description
- * 
+ * @description user private profile, content editor
+ *
  * @todo cleanup, keep default chunk in an external file, add additional markdown tutorial, etc...
- * 
+ *
  */
 
 'use strict';
 
 
 angular.module('cloudsqApp')
-
-    .controller('UserPrivateProfileCtrl', function($rootScope, $scope, $location) {
+    .controller('UserPrivateProfileCtrl', function($rootScope, $scope, $location, User) {
 
         var defaultChunk = [
             '# Chunk Headline',
@@ -31,21 +30,37 @@ angular.module('cloudsqApp')
         $rootScope.searchStatus = 'inactive';
         $rootScope.footerStatus = 'active';
 
-        // $scope.user.chunks = $scope.user.chunks || [];
+        $scope.user.chunks = $scope.user.chunks || [];
 
         $scope.user.chunk = {};
         $scope.user.chunk.setting = 'archive';
         $scope.user.chunk.content = defaultChunk;
 
+        /**
+         *
+         * @function save
+         *
+         */
+        $scope.save = function() {
 
-        // $scope.save = function() {
+            $scope.user.chunk.tstamp = +new Date();
+            $scope.user.chunks.push($scope.user.chunk);
 
-            // $scope.user.chunk.tstamp = +new Date();
-            // $scope.user.chunks.push($scope.user.chunk);
+            User.update($scope.user, function() {
+                $location.path('/user/' + $scope.user.username + '/');
+            });
 
-            // $scope.cloudSquareUser = new Project($scope.user);
-            // $scope.cloudSquareUser.update(function() {
-                // $location.path('/user/' + $scope.user.username + '/');
+        };
+
+        /**
+         *
+         * @function destroy
+         *
+         */
+        // $scope.destroy = function() {
+
+            // User.destroy($scope.user, function() {
+                // $location.path('/');
             // });
 
         // };
